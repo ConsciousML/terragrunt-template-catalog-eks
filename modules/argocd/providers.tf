d7fb2d0a1f53b1cfe93617f734bd1eb7b123dbde
Axel Mendoza <axel.mendoza@epita.fr>
2026-04-10 13:35:37 +0200

    remove(modules/argocd): auth variable, now takes only cluster_name

--- modules/argocd/providers.tf
+++ modules/argocd/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "3.0.1"
@@ -12,10 +16,18 @@ terraform {
   required_version = ">= 1.9.1"
 }
 
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
 provider "kubernetes" {
-  host                   = var.host
-  cluster_ca_certificate = var.cluster_ca_certificate
-  token                  = var.token
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
@@ -25,9 +37,9 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes = {
-    host                   = var.host
-    cluster_ca_certificate = var.cluster_ca_certificate
-    token                  = var.token
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
--- modules/argocd/variables.tf
+++ modules/argocd/variables.tf
@@ -1,18 +1,3 @@
-variable "host" {
-  description = "The hostname (in form of URI) of the Kubernetes API. Can be sourced from `KUBE_HOST`"
-  type        = string
-}
-
-variable "cluster_ca_certificate" {
-  description = "PEM-encoded root certificates bundle for TLS authentication. Can be sourced from `KUBE_CLUSTER_CA_CERT_DATA`"
-  type        = string
-}
-
-variable "token" {
-  description = "Token of your service account. Can be sourced from `KUBE_TOKEN`"
-  type        = string
-}
-
 variable "cluster_name" {
   description = "The name of the cluster"
   type        = string
