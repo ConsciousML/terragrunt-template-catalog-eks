85d1bf2bca3006441b6f52170b8340c5533cdc20
Axel Mendoza <axel.mendoza@epita.fr>
2026-04-10 12:34:55 +0200

    add(modules/argocd): helm chart version variable

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "9.5.0" # Specify the version for consistency
+  version          = var.helm_chart_version
   namespace        = "argocd"
   create_namespace = true
 }
--- modules/argocd/variables.tf
+++ modules/argocd/variables.tf
@@ -17,3 +17,9 @@ variable "cluster_name" {
   description = "The name of the cluster"
   type        = string
 }
+
+# https://artifacthub.io/packages/helm/argo/argo-cd
+variable "helm_chart_version" {
+  description = "The version of the helm chart of installing argocd"
+  type        = string
+}
