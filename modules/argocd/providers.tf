353bc76b8e32036f1aad22f9a97802f7aa047595
Axel Mendoza <axel.mendoza@epita.fr>
2026-04-10 14:46:16 +0200

    move(modules/): all versions to versions.tf

--- modules/argocd/providers.tf
+++ modules/argocd/providers.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "3.0.1"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.1.1"
-    }
-  }
-  required_version = ">= 1.9.1"
-}
-
 data "aws_eks_cluster" "this" {
   name = var.cluster_name
 }
---
+++ modules/argocd/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+  required_version = ">= 1.9.1"
+}
